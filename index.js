3e6d05c4ce4c24635e435dd6d1b3bd377a4f32c5
ravshanov-ramizjon <ravsanovramiz@gmail.com>
2024-05-31 19:04:59 -0930

    crispy-giggle

---
+++ index.js
@@ -0,0 +1,24 @@
+let num = prompt('Ведите любое число')
+
+if (num % 2 === 0) {
+     alert ('Wellcome')
+} else {
+    alert ('Sory')
+}
+
+
+/* let name = prompt('What is your name ?')
+
+if (name .startsWith`A`) {
+     alert ('Wellcome')
+} else {
+    alert ('Sory')
+} */
+
+/* let name = prompt('What is your name ?')
+
+if(name .length % 2 === 0) {
+     alert ('Wellcome')
+} else {
+    alert ('Sory')
+} */
